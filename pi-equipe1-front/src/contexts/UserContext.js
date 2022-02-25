c41f9214db904a793cd5c353be7abf0d2ea3da40
Alexander Marcelo Dos Reis De Almeida <helena.perdigueiro@gmail.com>
2022-02-25 10:40:38 -0300

    responsividade card recommendation

--- pi-equipe1-front/src/contexts/UserContext.js
+++ pi-equipe1-front/src/contexts/UserContext.js
@@ -5,11 +5,11 @@ export const UserContext = createContext();
 const UserContextProvider = ({ children }) => {
 
 
-        // let signed = localStorage.getItem("signed")
-        // ? JSON.parse(localStorage.getItem("signed"))
-        // : [];
+        let signed = localStorage.getItem("signed")
+        ? JSON.parse(localStorage.getItem("signed"))
+        : [];
 
-        let signed = JSON.parse(localStorage.getItem("signed"));
+        // let signed = JSON.parse(localStorage.getItem("signed"));
 
         useEffect(() => {
             localStorage.setItem('signed', JSON.stringify(signed));
