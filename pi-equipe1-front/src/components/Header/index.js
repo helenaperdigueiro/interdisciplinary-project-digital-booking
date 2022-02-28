81d6040f0c783a5e368449d188911ae1ce7c6923
Renata Leal <85504267+RenatadsLeal@users.noreply.github.com>
2022-02-28 05:19:45 -0300

    fontawesome

--- pi-equipe1-front/src/components/Header/index.js
+++ pi-equipe1-front/src/components/Header/index.js
@@ -2,6 +2,8 @@ import './style.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useUserContext } from '../../contexts/UserContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
 
@@ -12,7 +14,10 @@ const Header = () => {
             <Navbar id="header" className="fixed-top px-3 d-flex" collapseOnSelect expand="md" bg="light" variant="light">
                 <Navbar.Brand as={Link} to="/"><img id="logo" src="https://cdn-icons-png.flaticon.com/512/1659/1659303.png" alt="logo" /></Navbar.Brand>
                 <Nav.Link className="p-0 me-auto" as={Link} to="/"><div id="moto"><p>feels like</p><b> home.</b></div></Nav.Link>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle aria-controls="responsive-navbar-nav">
+      <FontAwesomeIcon
+        icon={faBars} color="#3ba9c2" size="lg"
+      /></Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                     <Nav >
                         <Nav.Link className="d-flex align-items-center" as={Link} to="/"><div id="avatar" className="me-1">CH</div><p className="m-0">Olá, Carol!</p></Nav.Link>
