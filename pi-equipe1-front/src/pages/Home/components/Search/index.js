2f3f49e97ecd669a6a8d2cb152d7efa83dd156b2
hakacarol <haka.carolina@gmail.com>
2022-03-03 16:01:07 -0300

    datepicker

--- pi-equipe1-front/src/pages/Home/components/Search/index.js
+++ pi-equipe1-front/src/pages/Home/components/Search/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
 import { Formik, Field, Form } from 'formik';
+import Media from 'react-media';
 import ptBr from "date-fns/locale/pt-BR"
 import "react-datepicker/dist/react-datepicker.css";
 import './style.css';
@@ -11,49 +12,59 @@ const Search = () => {
     const [dateRange, setDateRange] = useState([null, null]);
     const [startDate, endDate] = dateRange;
 
+    console.log(startDate);
+    console.log(endDate);
+
     return (
         <div id="search">
-            
-            <div id="searchBrowserTitle">
-                <h1>Buscar ofertas em hotéis, casas e muito mais</h1>
-            </div>
-            
-            <Formik
-                initialValues={{ city: '', startDate: {startDate}, endDate: {endDate},
-                }}
+            <div>
+                <Media queries={{
+                    small: "(max-width: 599px)",
+                    medium: "(min-width: 600px) and (max-width: 1199px)",
+                    large: "(min-width: 1200px)"
+                }}>
+                    {matches => (
+                        <>
+                            {matches.small && <p>I am small!</p>}
+                            {matches.medium && <p>I am medium!</p>}
+                            {matches.large && <p>I am large!</p>}
+                        </>
+                    )}
 
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2, startDate));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
-                <Form id="formSearch">
-                    <Field className="location" name="city" type="text" placeholder="Cidade" />
-                    
-                    <div className="date">
-                        
-                        <DatePicker
-            
-                            placeholderText="Check in - Check out"
-                            selectsRange={true}
-                            dateFormat="dd/MM/yyyy"
-                            minDate={new Date()}
-                            startDate={startDate}
-                            endDate={endDate}
-                            // monthsShown={2}n
-                            onChange={(update) => {
-                                setDateRange(update);
-                            }}
-                            locale="ptBr"
-                        />
+                    <div id="searchBrowserTitle">
+                        <h1>Buscar ofertas em hotéis, casas e muito mais</h1>
                     </div>
-                    
-                    <button className="buttonSearch" type="submit">Buscar</button>
-                </Form>
-            </Formik>
 
+                    <Formik
+                        initialValues={{
+                            city: '', startDate: null, endDate: null,
+                        }}
+                    >
+                        <Form id="formSearch">
+                            <Field className="location" name="city" type="text" placeholder="Cidade" />
+
+                            <div className="date">
+
+                                <DatePicker
+                                    placeholderText="Check in - Check out"
+                                    selectsRange={true}
+                                    dateFormat="dd/MM/yyyy"
+                                    minDate={new Date()}
+                                    startDate={startDate}
+                                    endDate={endDate}
+                                    // monthsShown={matches.medium ? 2:1}
+                                    onChange={(update) => {
+                                        setDateRange(update);
+                                    }}
+                                    locale="ptBr"
+                                />
+                            </div>
+
+                            <button className="buttonSearch" type="submit">Buscar</button>
+                        </Form>
+                    </Formik>
+                </Media>
+            </div>
         </div>
     );
 };
