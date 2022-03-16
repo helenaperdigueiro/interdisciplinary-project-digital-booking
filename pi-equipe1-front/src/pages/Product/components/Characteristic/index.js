196f7dbb241db8b4b33a54ebcabf824b41089dcf
Renata Leal <85504267+RenatadsLeal@users.noreply.github.com>
2022-03-16 18:04:45 -0300

    char icon size mod

--- pi-equipe1-front/src/pages/Product/components/Characteristic/index.js
+++ pi-equipe1-front/src/pages/Product/components/Characteristic/index.js
@@ -14,13 +14,13 @@ const Characteristic = () => {
                 <h5>O que este lugar oferece?</h5>
             </div>
             <div id="characteristics">
-                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faSink} size="xl" /> <p>Cozinha</p></div>
-                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faSnowflake} size="xl" /> <p>Ar Condicionado</p></div>
-                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faCar} size="xl" /> <p>Estacionamento</p></div>
-                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faWifi} size="xl" /> <p>Wifi</p></div>
-                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faPaw} size="xl" /> <p>Aceita Pets</p></div>
-                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faTv} size="xl" /> <p>Televisão</p></div>
-                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faPersonSwimming} size="xl" /> <p>Piscina</p></div>
+                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faSink} size="lg" /> <p>Cozinha</p></div>
+                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faSnowflake} size="lg" /> <p>Ar Condicionado</p></div>
+                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faCar} size="lg" /> <p>Estacionamento</p></div>
+                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faWifi} size="lg" /> <p>Wifi</p></div>
+                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faPaw} size="lg" /> <p>Aceita Pets</p></div>
+                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faTv} size="lg" /> <p>Televisão</p></div>
+                <div className='characteristic'><FontAwesomeIcon className="charIcons" icon={faPersonSwimming} size="lg" /> <p>Piscina</p></div>
             </div>
         </>
     );
