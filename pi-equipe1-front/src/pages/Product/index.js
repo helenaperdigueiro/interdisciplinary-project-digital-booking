235b86b521373968eff948432c5e392664293f8f
Alexander Marcelo Dos Reis De Almeida <helena.perdigueiro@gmail.com>
2022-03-17 22:56:50 -0300

    description and policy

--- pi-equipe1-front/src/pages/Product/index.js
+++ pi-equipe1-front/src/pages/Product/index.js
@@ -3,14 +3,20 @@ import ProductTitle from './components/ProductTitle';
 import Location from './components/Location';
 import Calendar from './components/Calendar';
 import Characteristic from './components/Characteristic';
+import Description from './components/Description';
+import Image from './components/Image';
+import Policy from './components/Policy';
 
 const Product = () => {
     return (
         <>
             <ProductTitle />
             <Location />
+            <Image />
+            <Description />
             <Characteristic/>
             <Calendar />
+            <Policy />
         </>
     );
 };
