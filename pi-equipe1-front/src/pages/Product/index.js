0ae52aedfdc1dac6a40f1d5d4084326d4e6b235a
hakacarol <haka.carolina@gmail.com>
2022-03-19 20:09:51 -0300

    components Product

--- pi-equipe1-front/src/pages/Product/index.js
+++ pi-equipe1-front/src/pages/Product/index.js
@@ -5,9 +5,11 @@ import { ProductContext } from '../../contexts/ProductContext';
 import ProductTitle from './components/ProductTitle';
 import Location from './components/Location';
 import Image from './components/Image';
+import Description from './components/Description';
 import Characteristic from './components/Characteristic';
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from 'react-helmet-async';
 import Calendar from './components/Calendar';
+import Policy from './components/Policy';
 
 const Product = () => {
 
@@ -31,9 +33,10 @@ const Product = () => {
             <ProductTitle />
             <Location />
             <Image />
+            <Description />
             <Characteristic/>
             <Calendar />
-
+            <Policy />
         </>
     );
 };
