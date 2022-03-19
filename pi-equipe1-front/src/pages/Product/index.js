bed6bde93c59f517505cd4cd64ce5c5984895fc8
hakacarol <haka.carolina@gmail.com>
2022-03-19 18:38:59 -0300

    component Image - Desktop

--- pi-equipe1-front/src/pages/Product/index.js
+++ pi-equipe1-front/src/pages/Product/index.js
@@ -1,16 +1,19 @@
 import './style.css';
 import ProductTitle from './components/ProductTitle';
 import Location from './components/Location';
-import Calendar from './components/Calendar';
+import Image from './components/Image';
 import Characteristic from './components/Characteristic';
+import Calendar from './components/Calendar';
 
 const Product = () => {
     return (
         <>
             <ProductTitle />
             <Location />
+            <Image />
             <Characteristic/>
             <Calendar />
+
         </>
     );
 };
