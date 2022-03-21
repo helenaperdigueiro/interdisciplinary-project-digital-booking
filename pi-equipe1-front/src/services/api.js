6a846b24dfa0ca0616452b17b7142458efc13c87
Alexander Marcelo Dos Reis De Almeida <helena.perdigueiro@gmail.com>
2022-03-21 16:06:43 -0300

    new ip backend

--- pi-equipe1-front/src/services/api.js
+++ pi-equipe1-front/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:8080'
-    baseURL: 'http://18.231.120.229:8080'
+    baseURL: 'http://15.228.221.184:8080/'
 });
 
 export default api;
