08607dd358c316178efe5250fa45a9220a5db6a2
hakacarol <haka.carolina@gmail.com>
2022-03-20 21:10:54 -0300

    component Image - mediaQuery

--- pi-equipe1-front/src/pages/Product/components/Image/index.js
+++ pi-equipe1-front/src/pages/Product/components/Image/index.js
@@ -15,11 +15,7 @@ const Image = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const mediaQueryDesktop = useMediaQuery({ minWidth: 825 });
-    const mediaQueryMobile = useMediaQuery({ maxWidth: 824 });
-
-    console.log(product);
-    // console.log(product.images);
+    const mediaQuery = useMediaQuery({ maxWidth: 825 });
 
     const imageProduct = [
         {
@@ -49,63 +45,63 @@ const Image = () => {
             thumbnail: "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         }
     ]
- 
-        return (
-            <div>
-                {mediaQueryDesktop &&
 
-                    <div id="galleryImage">
-                        <div id="galleryThumbnail">
-                            {
-                                product.images?.map( (productImage, index) => {
-                                    return (
-                                        <div className={`thumbnailProduct${index + 1}`} key={index}>
-                                            <img className="thumbnailImage" src={productImage.url} alt="" />
-                                        </div>
-                                    )
-                                })
-                            }
+    return (
+        <div>
+            {mediaQuery ?
 
-                            <button className="modalButton" variant="primary" onClick={handleShow}>
-                                Ver mais
-                            </button>
-                        </div>
+                <ImageGallery
+                    items={product.images}
+                    showThumbnails={false}
+                    thumbnailPosition={"bottom"}
+                    showPlayButton={false}
+                    showFullscreenButton={false}
+                    showIndex={true}
+                    slideInterval={3000}
+                />
 
-                        <div id="galleryModal">
-                            <Modal show={show} onHide={handleClose} size="lg" centered>
+                :
 
-                                <Modal.Header bsPrefix="modal-header" closeButton></Modal.Header>
-                                <Modal.Body bsPrefix="modal-body">
+                <div id="galleryImage">
+                    <div id="galleryThumbnail">
+                        {
+                            product.images?.map((productImage, index) => {
+                                return (
+                                    <div className={`thumbnailProduct${index + 1}`} key={index}>
+                                        <img className="thumbnailImage" src={productImage.url} alt="" />
+                                    </div>
+                                )
+                            })
+                        }
 
-                                    <ImageGallery
-                                        items={imageProduct}
-                                        showThumbnails={true}
-                                        thumbnailPosition={"bottom"}
-                                        showPlayButton={false}
-                                        showFullscreenButton={true}
-                                        showIndex={true}
-                                    />
-                                </Modal.Body>
-
-                            </Modal>
-                        </div>
+                        <button className="modalButton" variant="primary" onClick={handleShow}>
+                            Ver mais
+                        </button>
                     </div>
-                }
 
-                {mediaQueryMobile &&
-                    <ImageGallery
-                        items={imageProduct}
-                        showThumbnails={false}
-                        thumbnailPosition={"bottom"}
-                        showPlayButton={false}
-                        showFullscreenButton={false}
-                        showIndex={true}
-                        slideInterval={3000}
-                    />
-                }
-            </div>
-        )
-    
+                    <div id="galleryModal">
+                        <Modal show={show} onHide={handleClose} size="lg" centered>
+
+                            <Modal.Header bsPrefix="modal-header" closeButton></Modal.Header>
+                            <Modal.Body bsPrefix="modal-body">
+
+                                <ImageGallery
+                                    items={product.images}
+                                    showThumbnails={true}
+                                    thumbnailPosition={"bottom"}
+                                    showPlayButton={false}
+                                    showFullscreenButton={true}
+                                    showIndex={true}
+                                />
+                            </Modal.Body>
+
+                        </Modal>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+
 }
 
 export default Image;
