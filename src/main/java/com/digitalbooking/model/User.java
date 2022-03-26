cfd630c26c6936d3e489fca0a02c87a341c88436
Leonardo <10939726-leonardosantino@users.noreply.gitlab.com>
2022-03-26 09:26:44 -0300

    add User model

---
+++ src/main/java/com/digitalbooking/model/User.java
@@ -0,0 +1,27 @@
+package com.digitalbooking.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String lastName;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+}
