beda16054073115c8005b4b7eae6d6f8b6fe71ad
helenaperdigueiro <helena.perdigueiro@gmail.com>
2022-03-28 13:55:04 -0300

    calendar availability component

--- pi-equipe1-front/src/pages/Product/components/Calendar/index.js
+++ pi-equipe1-front/src/pages/Product/components/Calendar/index.js
@@ -1,33 +1,13 @@
 import './style.css';
-import React, { useState } from "react";
-import DatePicker, { registerLocale } from "react-datepicker";
-import { useMediaQuery } from 'react-responsive';
-import "react-datepicker/dist/react-datepicker.css";
+import CalendarAvailability from '../../../../components/CalendarAvailability';
 
 const Calendar = () => {
-    const [dateRange, setDateRange] = useState([null, null]);
-    const [startDate, endDate] = dateRange;
-    const mediaQuery = useMediaQuery({ minWidth: 750 });
 
     return (
         <div className="availableDates">
             <h2>Datas disponíveis</h2>
             <div className="calendarSelection">
-                <div className="calendar">
-                <DatePicker
-                    selectsRange={true}
-                    dateFormat="dd/MM/yyyy"
-                    minDate={new Date()}
-                    startDate={startDate}
-                    endDate={endDate}
-                    monthsShown={mediaQuery ? 2 : 1}
-                    onChange={(update) => {
-                        setDateRange(update);
-                    }}
-                    locale="ptBr"
-                    inline
-                />
-                </div>
+                <CalendarAvailability />
                 <div className="selection">
                     <p>Adicione as datas da sua viagem para obter preços exatos</p>
                     <button>Iniciar reserva</button>
