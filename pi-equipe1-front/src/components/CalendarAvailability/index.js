2106066dd4cf05744c465fca23ad16e0aaf8be7e
hakacarol <haka.carolina@gmail.com>
2022-03-30 19:08:19 -0300

    update

--- pi-equipe1-front/src/components/CalendarAvailability/index.js
+++ pi-equipe1-front/src/components/CalendarAvailability/index.js
@@ -1,27 +1,16 @@
 import './style.css';
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import { useMediaQuery } from 'react-responsive';
 import "react-datepicker/dist/react-datepicker.css";
 import { useDateRangeContext } from '../../contexts/DateRangeContext';
-import { useProductContext } from '../../contexts/ProductContext';
 
 const CalendarAvailability = () => {
 
-    const { product } = useProductContext();
-
-    console.log(product.reservations[0]?.startDate);
-
     const [dateRange, setDateRange] = useState([null, null]);
-
     const [startDate, endDate] = dateRange;
     const { setDateReservation } = useDateRangeContext()
-    
     const mediaQuery = useMediaQuery({ minWidth: 750 });
-
-    console.log(product)
-    console.log(product.reservations)
-    //console.log(product.reservations[0]?.startDate)
 
     return (
         <div className="calendar">
