8869f2eea48468674175cf18ca80adfde2695ffa
helenaperdigueiro <helena.perdigueiro@gmail.com>
2022-04-11 12:08:35 -0300

    characteristic service and cotroller

--- src/main/java/com/digitalbooking/repository/CharacteristicRepository.java
+++ src/main/java/com/digitalbooking/repository/CharacteristicRepository.java
@@ -1,9 +1,10 @@
 package com.digitalbooking.repository;
 
+import com.digitalbooking.model.Characteristic;
 import com.digitalbooking.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CharacteristicRepository extends JpaRepository<Image, Integer> {
+public interface CharacteristicRepository extends JpaRepository<Characteristic, Integer> {
 }
