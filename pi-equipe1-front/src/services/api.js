e2690ca96b1f2b3c8d3cf7a9de67a5446d0b2480
helenaperdigueiro <helena.perdigueiro@gmail.com>
2022-04-13 18:04:17 -0300

    new ip backend

--- pi-equipe1-front/src/services/api.js
+++ pi-equipe1-front/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:8080/'
-    baseURL: 'http://18.231.197.217:8080/'
+    baseURL: 'http://54.193.249.48:8080/'
 });
 
 export default api;
